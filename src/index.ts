b8d4cc30c3c3aa922ad44afcfd7ae5eeaabc8009
krisnapy <krisnapy533@gmail.com>
2025-03-22 23:05:28 +0800

    Update dependencies and enhance API with Swagger documentation and rate limiting

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,33 @@
+import swagger from "@elysiajs/swagger";
 import { Elysia } from "elysia";
 
 import routes from "./routes";
+import { rateLimit } from "elysia-rate-limit";
 
 const app = new Elysia();
 
-app.use(routes).listen(9091);
+app
+  .use(
+    swagger({
+      path: "/api/v1/docs",
+      documentation: {
+        info: {
+          title: "API Documentation",
+          version: "1.0.0",
+        },
+      },
+    })
+  )
+  .use(
+    rateLimit({
+      max: 50,
+      skip: (ctx) => {
+        return ctx.url.includes("/api/v1/docs");
+      },
+    })
+  )
+  .use(routes)
+  .listen(9091);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
