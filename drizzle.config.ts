5745c45ace9dcfd5a35aa688fd871220f19f421e
krisnapy <krisnapy533@gmail.com>
2025-03-23 02:43:22 +0800

    chore: Update drizzle configuration and package scripts for migration

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,13 +2,14 @@ import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
   schema: './src/db/schemas/*',
-  driver: 'pg',
+  dialect: 'postgresql',
   dbCredentials: {
     database: process.env.DATABASE_NAME,
     host: process.env.DATABASE_HOST,
     password: process.env.DATABASE_PASSWORD,
     port: Number(process.env.DATABASE_PORT),
     user: process.env.DATABASE_USERNAME,
+    ssl: process.env.DATABASE_HOST === 'localhost' ? false : true,
   },
   verbose: false,
   strict: false,
